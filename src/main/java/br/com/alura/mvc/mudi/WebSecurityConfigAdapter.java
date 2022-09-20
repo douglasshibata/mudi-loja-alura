d0f7acae3db2e153c9e834e01c57b40658904301
Douglas Shibata <douglas.shibata11@gmail.com>
2022-09-20 09:35:11 -0300

    Relacionando pedido com usuario

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfigAdapter.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfigAdapter.java
@@ -1,18 +1,26 @@
 package br.com.alura.mvc.mudi;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     /*
      * Autorização
@@ -26,6 +34,7 @@ public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin(form -> form
                         .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
                         .permitAll())
                 .logout(logout -> logout.logoutUrl("/logout"));
     }
@@ -34,11 +43,17 @@ public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
      * Autenticação
      * Configurando usuário e senha inicial para teste
      */
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder().username("Teste").password("teste").roles("ADM")
-                .build();
-        return new InMemoryUserDetailsManager(userDetails);
-    }
+  	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		auth
+			.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder);
+            UserDetails user =
+            				 User.builder()
+            					.username("teste")
+            					.password(encoder.encode("teste"))
+            					.roles("ADM")
+            					.build();
 }
